6414aa0f12cf098bf6df054b548ac38374de3ddc
Tomaz Saraiva <tomaz.saraiva@gmail.com>
2024-04-30 22:58:09 +0100

    add XML documentation to pagecontainer

--- Assets/PageSlider/Scripts/PageContainer.cs
+++ Assets/PageSlider/Scripts/PageContainer.cs
@@ -8,19 +8,35 @@ using UnityEditor;
 
 namespace TS.PageSlider
 {
+    /// <summary>
+    /// This class represents a container for a page in a paginated view.
+    /// It handles assigning content to the container and manages the active state of the contained page.
+    /// </summary>
     public class PageContainer : MonoBehaviour
     {
         #region Variables
 
         [Header("Children")]
+
+        // <summary>
+        /// The PageView component representing the content of this page container.
+        /// </summary>
+        [Tooltip("The PageView component representing the content of this page container")]
         [SerializeField] private PageView _page;
 
         #endregion
 
+        /// <summary>
+        /// Assigns content (RectTransform) to this container.
+        /// If no content is provided, it creates a new GameObject with a RectTransform and a PageView component.
+        /// The assigned content is then parented to this container and its properties are set to ensure proper positioning and scaling.
+        /// </summary>
+        /// <param name="content">The RectTransform representing the content to be assigned.</param>
         public void AssignContent(RectTransform content)
         {
             if (content == null)
             {
+                // Create a new GameObject with required components if content is not provided.
                 var contentObject = new GameObject("Content", typeof(RectTransform), typeof(PageView));
                 content = contentObject.GetComponent<RectTransform>();
             }
@@ -42,14 +58,28 @@ namespace TS.PageSlider
 #endif
         }
 
+        /// <summary>
+        /// Calls the ChangingToActiveState method on the contained PageView component,
+        /// to signal a transition to an active state.
+        /// </summary>
         public void ChangingToActiveState()
         {
             _page.ChangingToActiveState();
         }
+
+        /// <summary>
+        /// Calls the ChangingToInactiveState method on the contained PageView component,
+        /// to signal a transition to an inactive state.
+        /// </summary>
         public void ChangingToInactiveState()
         {
             _page.ChangingToInactiveState();
         }
+
+        /// <summary>
+        /// Calls the ChangeActiveState method on the contained PageView component with the provided active state.
+        /// </summary>
+        /// <param name="active">True to set the page to active, False to set it to inactive.</param>
         public void ChangeActiveState(bool active)
         {
             _page.ChangeActiveState(active);
