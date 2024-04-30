145fc4bc9f7cf2615c13c439678378d8f2ced309
Tomaz Saraiva <tomaz.saraiva@gmail.com>
2024-04-30 22:36:22 +0100

    add XML documentation to pageslider

--- Assets/PageSlider/Scripts/PageSlider.cs
+++ Assets/PageSlider/Scripts/PageSlider.cs
@@ -10,19 +10,42 @@ using UnityEditor;
 
 namespace TS.PageSlider
 {
+    /// <summary>
+    /// The PageSlider class manages a collection of pages within a PageScroller component. 
+    /// It provides functionality for adding, removing, and keeping track of pages, 
+    /// as well as handling page change events.
+    /// </summary>
     public class PageSlider : MonoBehaviour
     {
         #region Variables
 
         [Header("References")]
+
+        /// <summary>
+        /// "An optional reference to a PageDotsIndicator component used to display dots for each page."
+        /// </summary>
+        [Tooltip("Optional reference to a PageDotsIndicator to display dots for each page")]
         [SerializeField] private PageDotsIndicator _dotsIndicator;
 
         [Header("Children")]
+
+        /// <summary>
+        /// A list of PageContainer components representing the pages managed by the PageSlider.
+        /// </summary>
+        [Tooltip("A list of PageContainer components representing the pages managed by the PageSlider")]
         [SerializeField] private List<PageContainer> _pages;
 
         [Header("Events")]
+
+        /// <summary>
+        /// Invoked whenever the active page changes. 
+        /// The event argument is a reference to the new active page.
+        /// </summary>
         public UnityEvent<PageContainer> OnPageChanged;
 
+        /// <summary>
+        /// Gets the rectangle of the PageSlider component.
+        /// </summary>
         public Rect Rect { get { return ((RectTransform)transform).rect; } }
 
         private PageScroller _scroller;
@@ -39,6 +62,12 @@ namespace TS.PageSlider
             _scroller.OnPageChangeEnded.AddListener(PageScroller_PageChangeEnded);
         }
 
+
+        /// <summary>
+        /// Adds a new page to the PageSlider. 
+        /// The content argument specifies the RectTransform of the content to be displayed on the new page.
+        /// </summary>
+        /// <param name="content">The RectTransform of the content to be displayed on the new page.</param>
         public void AddPage(RectTransform content)
         {
             if (_scroller == null)
@@ -77,6 +106,10 @@ namespace TS.PageSlider
             EditorUtility.SetDirty(this);
 #endif
         }
+
+        /// <summary>
+        /// Removes all pages from the PageSlider and clears the associated PageDotsIndicator (if exists).
+        /// </summary>
         public void Clear()
         {
             for (int i = 0; i < _pages.Count; i++)
@@ -96,11 +129,24 @@ namespace TS.PageSlider
             }
         }
 
+
+        /// <summary>
+        /// Called by the PageScroller component when a page change starts. 
+        /// Deactivates the page at the fromIndex and activates the page at the toIndex.
+        /// </summary>
+        /// <param name="fromIndex">The index of the page that is being deactivated.</param>
+        /// <param name="toIndex">The index of the page that is being activated.</param>
         private void PageScroller_PageChangeStarted(int fromIndex, int toIndex)
         {
             _pages[fromIndex].ChangingToInactiveState();
             _pages[toIndex].ChangingToActiveState();
         }
+
+        /// <summary>
+        /// Called by the PageScroller component when a page change ends. Sets the page at the fromIndex to inactive and the page at the toIndex to active. Updates the PageDotsIndicator and invokes the OnPageChanged event.
+        /// </summary>
+        /// <param name="fromIndex">The index of the page that is being deactivated.</param>
+        /// <param name="toIndex">The index of the page that is being activated.</param>
         private void PageScroller_PageChangeEnded(int fromIndex, int toIndex)
         {
             _pages[fromIndex].ChangeActiveState(false);
@@ -114,6 +160,10 @@ namespace TS.PageSlider
             OnPageChanged?.Invoke(_pages[toIndex]);
         }
 
+        /// <summary>
+        /// Finds the PageScroller component within the children of the gameobject this script is attached to. 
+        /// </summary>
+        /// <returns>The PageScroller component found in the children, or null if not found.</returns>
         private PageScroller FindScroller()
         {
             var scroller = GetComponentInChildren<PageScroller>();
